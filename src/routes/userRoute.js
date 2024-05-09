8e67a907eb0359b59ed19ce42b05e2ba00aa0bb8
Padam7890 <padamthapamoho@gmail.com>
2024-05-09 20:54:35 +0545

    new changes

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -5,9 +5,9 @@ const refreshTokenCheck = require("../controllers/user/refreshtoken.controller")
 const login = require("../controllers/user/login.controller");
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login',login)
-router.get('/', getallusers)
+router.post('/register',  register);
+router.post('/login', login)
+router.get('/',  getallusers)
 router.patch('/refresh_token/:refreshToken',refreshTokenCheck);
 
 module.exports = router;
